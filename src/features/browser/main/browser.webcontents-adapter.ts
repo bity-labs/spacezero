4587f5ac4c0ae84c65840af946e55acbc4447206
tiby <101003754+0xtiby@users.noreply.github.com>
2026-07-26 08:47:25 +0200

    fix(review): address PR #223 round 1 feedback

--- src/features/browser/main/browser.webcontents-adapter.ts
+++ src/features/browser/main/browser.webcontents-adapter.ts
@@ -209,6 +209,7 @@ export class ElectronBrowserViewAdapter implements BrowserViewAdapter {
         browserSession.clearStorageData({
           storages: [
             'cookies',
+            'filesystem',
             'localstorage',
             'indexdb',
             'serviceworkers',
